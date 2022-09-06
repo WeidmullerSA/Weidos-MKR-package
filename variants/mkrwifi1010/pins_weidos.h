4338b6e2458b3aec7510605f9b74529e211b7608
OriolFalip <Oriol.Falip@weidmueller.com>
2022-09-06 13:17:11 +0200

    Sort pin numbers in pins_weidos
    Nothing was added or deleted. Pin definitions have been moved so as the appear sorted according to its pin number.

--- variants/mkrwifi1010/pins_weidos.h
+++ variants/mkrwifi1010/pins_weidos.h
@@ -2,25 +2,6 @@
 #define PINS_WEIDOS_H
 
 //####Weidmuller####
-//Digital inputs
-#define DI_4 (25u)
-#define DI_5 (1u)
-#define DI_6 (2u)
-#define DI_7 (3u)
-static const uint32_t pin25 = DI_4;
-static const uint32_t pin26 = DI_5;
-static const uint32_t pin27 = DI_6;
-static const uint32_t pin28 = DI_7;
-
-//Digital outputs
-#define DO_0 (0x2100)
-#define DO_1 (0x2101)
-#define DO_2 (0x2102)
-#define DO_3 (0x2103)
-static const uint32_t pin55 = DO_0;
-static const uint32_t pin56 = DO_1;
-static const uint32_t pin57 = DO_2;
-static const uint32_t pin58 = DO_3;
 
 //Analog/Digital inputs
 #define ADI_0 (17u)
@@ -32,15 +13,41 @@ static const uint32_t pin16 = ADI_1;
 static const uint32_t pin17 = ADI_2;
 static const uint32_t pin18 = ADI_3;
 
-//Analog output
-#define AO_0 (15u)
-static const uint32_t pin45 = AO_0;
+//Digital inputs
+#define DI_4 (25u)
+#define DI_5 (1u)
+#define DI_6 (2u)
+#define DI_7 (3u)
+static const uint32_t pin25 = DI_4;
+static const uint32_t pin26 = DI_5;
+static const uint32_t pin27 = DI_6;
+static const uint32_t pin28 = DI_7;
 
 //Multifunction pins
 #define MF_0    19
 #define MF_1    0
 static const uint32_t pin41 = MF_0;
 static const uint32_t pin42 = MF_1;
+
+//Analog output
+#define AO_0 (15u)
+static const uint32_t pin45 = AO_0;
+
+//Digital outputs
+#define DO_0 (0x2100)
+#define DO_1 (0x2101)
+#define DO_2 (0x2102)
+#define DO_3 (0x2103)
+static const uint32_t pin55 = DO_0;
+static const uint32_t pin56 = DO_1;
+static const uint32_t pin57 = DO_2;
+static const uint32_t pin58 = DO_3;
+
+
+
+
+
+
 
 #define HAVE_MCP23008
 
